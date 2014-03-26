b60f29aea2f8a01d2032c35ace3d1ee60a8010a9
Mikael Grön <mikael.gron@duva.se>
2014-03-26 13:56:46 +0100

    Who uses soft tabs in 2014?

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,56 +4,56 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+	"use strict";
 
-    $(document).ready(function(){
+	$(document).ready(function(){
 
-        $(".post-content").fitVids();
+		$(".post-content").fitVids();
 
-        function poltergeistFullImg() {
-            $("img").each( function() {
-                var contentWidth = $(".post-content").outerWidth(); // Width of the content
-                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+		function poltergeistFullImg() {
+			$("img").each( function() {
+				var contentWidth = $(".post-content").outerWidth(); // Width of the content
+				var imageWidth = $(this)[0].naturalWidth; // Original image resolution
 
-                if (imageWidth >= contentWidth) {
-                    $(this).addClass('full-img');
-                } else {
-                    $(this).removeClass('full-img');
-                }
-            });
-        };
+				if (imageWidth >= contentWidth) {
+					$(this).addClass('full-img');
+				} else {
+					$(this).removeClass('full-img');
+				}
+			});
+		};
 
-        poltergeistFullImg();
-        $(window).smartresize(poltergeistFullImg);
+		poltergeistFullImg();
+		$(window).smartresize(poltergeistFullImg);
 
-    });
+	});
 
 }(jQuery));
 
 (function($,sr){
 
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  var debounce = function (func, threshold, execAsap) {
-      var timeout;
+	// debouncing function from John Hann
+	// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+	var debounce = function (func, threshold, execAsap) {
+		var timeout;
 
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
+		return function debounced () {
+			var obj = this, args = arguments;
+			function delayed () {
+				if (!execAsap)
+					func.apply(obj, args);
+				timeout = null;
+			};
 
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
+			if (timeout)
+				clearTimeout(timeout);
+			else if (execAsap)
+				func.apply(obj, args);
 
-          timeout = setTimeout(delayed, threshold || 100);
-      };
-  }
-  // smartresize
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+			timeout = setTimeout(delayed, threshold || 100);
+		};
+	}
+	// smartresize
+	jQuery.fn[sr] = function(fn){	return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
 })(jQuery,'smartresize');
